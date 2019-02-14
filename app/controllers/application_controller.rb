d3062b6a28a2811ec20bec8f54ea36581bda96ae
toruchan <ishikawatoru01@gmail.com>
2019-02-14 18:27:37 +0900

    認証ユーザによってリダイレクトパスを分けるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    new_test_path
+    if resource.instance_of?(User)
+      new_test_path
+    elsif resource.instance_of?(AdminUser)
+      root_path
+    end
   end
 end
