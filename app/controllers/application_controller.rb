53d69b15be17f1c9475bc99afbd79477697ae146
toruchan <ishikawatoru01@gmail.com>
2019-02-23 12:37:34 +0900

    git管理不要なものを削除

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    if resource.instance_of?(User)
-      new_test_path
-    elsif resource.instance_of?(AdminUser)
-      root_path
-    end
-  end
-end
