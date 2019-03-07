c9b8c07b0917f5c7e81e21c381dbc6e5c0826757
toruchan <ishikawatoru01@gmail.com>
2019-03-07 20:56:51 +0900

    動画アップロード機能の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_134652) do
+ActiveRecord::Schema.define(version: 2019_03_07_113415) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2019_03_05_134652) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "movie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
