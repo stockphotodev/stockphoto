c657a8f32cfdc8ad66edc7cb3842b99699c2d4b9
toruchan <ishikawatoru01@gmail.com>
2019-03-17 16:24:29 +0900

    イラストの一覧ページ、詳細ページ作成

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -1,2 +1,15 @@
 class IllustrationsController < ApplicationController
+  before_action :set_image, only: [:show]
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    illustration_ids = IllustrationCategory.where(category_id: params[:format].to_i).pluck(:illustration_id)
+    @illustrations = Illustration.where(id: illustration_ids)
+  end
+
+  private
+
+  def set_illustration
+    @illustration = Illustration.find(params[:id])
+  end
 end
