69018999fec16f6e2ab0dcd4184a82cdacc70138
toruchan <ishikawatoru01@gmail.com>
2019-06-22 19:49:19 +0900

    TOPの検索フォームの処理実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,21 @@
+# coding: utf-8
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @images = Image.last(6)
-    @illustrations = Illustration.last(6)
-    @movies = Movie.last(6)
+
+    keyword = params[:keyword].present? ? params[:keyword] : ""
+
+    # TODO:runsackにしたい
+    if keyword.blank?
+      @images = Image.last(6)
+      @illustrations = Illustration.last(6)
+      @movies = Movie.last(6)
+    else
+      @images = Image.where("name LIKE ?", "%#{keyword}%").last(6)
+      @illustrations = Illustration.where("name LIKE ?", "%#{keyword}%").last(6)
+      @movies = Movie.where("name LIKE ?", "%#{keyword}%").last(6)
+    end
   end
 
 end
