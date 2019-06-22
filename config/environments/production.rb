953c931af387d75f221a807f9f970cba50c64e53
toruchan <ishikawatoru01@gmail.com>
2019-06-22 18:57:51 +0900

    portの指定削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'pixagraph.net', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'pixagraph.net' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
