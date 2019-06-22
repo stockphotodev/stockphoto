b872c531f5c51b987e2ba1efc53f085aa8176b2b
toruchan <ishikawatoru01@gmail.com>
2019-06-22 18:54:47 +0900

    メールの設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'pixagraph.net' }
+  config.action_mailer.default_url_options = { host: 'pixagraph.net', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
 end
