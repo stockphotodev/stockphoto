fdab8e5773d1c4ee12ad2ffcf1c3565d4a23ab81
toruchan <ishikawatoru01@gmail.com>
2019-11-16 19:24:27 +0900

    降順で表示するように修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    keyword = params[:keyword].present? ? params[:keyword] : ""
-        
-    # TODO:runsackにしたい
-    if keyword.blank?
-      @images = Image.last(6)
-      @illustrations = Illustration.last(6)
-      @movies = Movie.last(6)
-    else
-      @images = Image.where("name LIKE ?", "%#{keyword}%").last(6)
-      @illustrations = Illustration.where("name LIKE ?", "%#{keyword}%").last(6)
-      @movies = Movie.where("name LIKE ?", "%#{keyword}%").last(6)
-    end
+    @images = Image.last(6).reverse
+    @illustrations = Illustration.last(6).reverse
+    @movies = Movie.last(6).reverse
   end
 
   def search
@@ -23,11 +14,11 @@ class HomeController < ApplicationController
 
     case type
     when '画像' then
-      @images = Image.where("name LIKE ?", "%#{keyword}%").last(6)
+      @images = Image.where("name LIKE ?", "%#{keyword}%").last(6).reverse
     when 'イラスト' then
-      @illustrations = Illustration.where("name LIKE ?", "%#{keyword}%").last(6)
+      @illustrations = Illustration.where("name LIKE ?", "%#{keyword}%").last(6).reverse
     when '動画' then
-      @movies = Movie.where("name LIKE ?", "%#{keyword}%").last(6)
+      @movies = Movie.where("name LIKE ?", "%#{keyword}%").last(6).reverse
     end
   end
 end
