6688d3489ef873377afdf53ab61863dead0741fd
toruchan <ishikawatoru01@gmail.com>
2019-11-28 15:29:35 +0900

    モーダル間のパラメータ引き継ぎ処理の実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,10 @@ $( function() {
 
     // アカウント登録から契約手続きへのモーダル切り替え
     $('#registrationToAgreement').click( function () {
+        button = document.getElementById('registrationToAgreement');
+        button.dataset.email = $('#em').val();
+        button.dataset.pass = $('#pass').val();
+        button.dataset.pid = $('#pid').val();
         $('#registrationModal').modal('hide');
     });
 
@@ -52,15 +56,45 @@ $( function() {
 
     // 規約同意のチェックボックスの状態によってボタンを切り替え
     $('#agreementCheck').click( function () {
-        var btn = document.getElementById('agreementToThanks');
+        var btn = document.getElementById('submit');
         if ($('#agreementCheck').prop('checked')) {
             btn.removeAttribute('disabled');
         } else {
             btn.setAttribute('disabled', 'disabled');
         }
     });
-    
+
+    // モーダル上のプラン選択のラジオボタン切り替え
     $("[class^='plan_']").click( function () {
-        $(`.${$(this).attr('class')}`).prop('checked',true);
+        $(`#${$(this).attr('class')}`).prop('checked',true);
+        button = document.getElementById('planToRegistration');
+        button.dataset.pid = $(`#${$(this).attr('class')}`).val();
     });
+
+    $('#registrationModal').on('show.bs.modal', function (event) {
+        // ボタンを取得
+        var button = $(event.relatedTarget);
+        // data-***の部分を取得
+        var button_data = button.data('pid');
+        var modal = $(this);
+        // モーダルに取得したパラメータを表示
+        // 以下ではh5のモーダルタイトルのクラス名を取得している
+        modal.find('#pid').val(button_data);
+    })
+
+    $('#agreementModal').on('show.bs.modal', function (event) {
+        // ボタンを取得
+        var button = $(event.relatedTarget);
+        // data-***の部分を取得
+        var em_data = button.data('email');
+        var pass_data = button.data('pass');
+        var pid_data = button.data('pid');
+        var modal = $(this);
+        // モーダルに取得したパラメータを表示
+        // 以下ではh5のモーダルタイトルのクラス名を取得している
+        modal.find('#hem').val(em_data);
+        modal.find('#hpass').val(pass_data);
+        modal.find('#hpid').val(pid_data);
+    })
+
 });
